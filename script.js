4416f34b14464ec88f6638b1fb26909bf9772c40
xTHEZEQUEx <nicolassequera8@gmail.com>
2023-08-23 17:54:09 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let h1 = document.querySelector('h1');
+let p = document.querySelector('p');
+let pClase = document.querySelector('.parrafoClase');
+let pId = document.querySelector('#parrafoId');
+let input = document.querySelector('input');
+
+console.log(input.value);
+
+console.log({
+    h1,
+    p,
+    pClase,
+    pId,
+    input
+})
